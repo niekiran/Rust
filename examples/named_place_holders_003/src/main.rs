539ad7eef7fc59e504bac72d69b16b19251d9828
Kiran Nayak <kiran@bharatisoft.in>
2024-04-15 11:34:14 +0530

    Update programs

--- examples/named_place_holders_003/src/main.rs
+++ examples/named_place_holders_003/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let name = "John";
     let age = 30;
 
-    //palce holders( '{}' ) with out names
+    //palce holders( '{}' ) without names
     let msg = format!("I am {} and I am {} years old", name, age);
 
     //place holders( '{}' ) with given names 'user_name' and 'user_age'
@@ -10,5 +10,15 @@ fn main() {
         "I am {user_name} and I am {user_age} years old",
         user_name = name,
         user_age = age
+    );//This code can also be written like below 
+
+    // You can use a shorthand syntax for variables in 
+    // formatted strings when the variable names are the same as the names of the 
+    // placeholders
+    let msg = format!(
+        "I am {name} and I am {age} years old"
     );
+
+    println!("{}", msg);
+
 }
--- examples/using_rust_tools_004/src/main.rs
+++ examples/using_rust_tools_004/src/main.rs
@@ -1,11 +1,38 @@
 
+/* Activate this code block and run 'cargo clippy' and 
+observe the errors/suggestions
+    
+fn main() {
+    let pi = 3.14;
+    let area = pi * 4.0 * 4.0;
+    println!("{}", area);
+}
 
-fn calc_circle_area(_radius: f32) -> f32 {
+*/
+
+/* This code block generates warnings related to unused variabels 
+   and unused functions and when you run the command 'cargo fix --allow-dirty' 
+   some warnings are auto fixed 
+
+fn calc_circle_area(radius: f32) -> f32 {
     3.14 * 4.0 * 4.0
 }
 
 fn main() {
-    let pi = std::f32::consts::PI;
-    let _area = pi * 4.0 * 4.0;
+    let pi = 3.14;
+    let area = pi * 4.0 * 4.0;
     println!("{}", calc_circle_area(10.0));
 }
+ */
+
+ /* 
+    Running 'cargo build' does not encounter any issues with this code. 
+    but when you run 'cargo clippy', it suggests using iterators, 
+    which is the idiomatic way to handle iteration in Rust.
+ */
+ fn main() {
+    let array = [1, 2, 3];
+    for i in 0..=2 {
+        println!("{}", array[i]);
+    }
+ }
